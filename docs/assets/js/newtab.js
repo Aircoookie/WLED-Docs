f5a71663809410c2a586c6db64d58d2ab6e33710
Tyler Meuse <9298659+tmeuze@users.noreply.github.com>
2021-05-21 15:11:39 -0400

    test-normalizing

---
+++ docs/assets/js/newtab.js
@@ -0,0 +1,12 @@
+(function() {
+  var links = document.links;
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = "_blank";
+      links[i].setAttribute("rel", "noopener noreferrer");
+      links[i].className += " externalLink";
+    } else {
+      links[i].className += " localLink";    
+    }
+  }
+})();
